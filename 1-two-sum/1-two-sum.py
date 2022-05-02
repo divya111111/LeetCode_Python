ad08cf310656cf99a89143c99753f5e29ec96db7
DIVYA SHARMA <66716367+divya111111@users.noreply.github.com>
2022-05-02 17:33:40 +0530

    Time: 67 ms (66.53%), Space: 14.4 MB (24.36%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,21 +1,12 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        list1=[]
-        n=len(nums)
-        for i in range(0,n):
-            for j in range(i+1,n):
-                # print(i)
-                # print(j)
-                sum1=nums[i]+nums[j]
-                if(sum1 == target):
-                    list1.append(i)
-                    list1.append(j)
-                
-        return list1
+        diction=dict()
+        for index, element in enumerate(nums):
+            comp=target-element
+            if comp in diction:
+                return [index,diction[comp]]
             
-        
+            diction[element]=index   # update key and value in dict
+        return []
+    
+            
